530f2a1244a5ec24fc934fb4e3dffb2d93307c27
Alfons <ajuan@dsic.upv.es>
2023-03-12 14:23:21 +0100

    ...

--- 02_fun/8.2 Métodos de primer orden/8.2.2.gd.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-grad, theta, eta, tol = lambda t: 2*t, 10.0, 0.2, 0.01
-while True:
-	delta = -eta * grad(theta)
-	if np.all(np.abs(delta) <= tol):
-		break
-	theta += delta
-	print(round(theta, 3))
